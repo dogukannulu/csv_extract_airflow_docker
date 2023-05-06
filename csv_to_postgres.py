56a5fccf6bf03f7d6b97b1fb95e21a46dda4480a
Dogukan Ulu <dogukannulu@gmail.com>
2023-05-06 15:11:21 +0300

    add port, create df

--- csv_to_postgres.py
+++ csv_to_postgres.py
@@ -7,7 +7,7 @@ postgres_host = os.environ.get('postgres_host')
 postgres_database = os.environ.get('postgres_database')
 postgres_user = os.environ.get('postgres_user')
 postgres_password = int(os.environ.get('postgres_password'))
-
+postgres_port = int(os.environ.get('postgres_port'))
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
@@ -20,7 +20,8 @@ conn = psycopg2.connect(
     host=postgres_host,
     database=postgres_database,
     user=postgres_user,
-    password=postgres_password
+    password=postgres_password,
+    port=postgres_port
 )
 
 # Create a cursor object to execute SQL queries
--- from_postgres_modify.py
+++ from_postgres_modify.py
@@ -1,5 +1,41 @@
+import psycopg2
+import os
+import logging
 import pandas as pd
 
-df = pd.read_csv('Traffic_Flow_Map_Volumes.csv')
+postgres_host = os.environ.get('postgres_host')
+postgres_database = os.environ.get('postgres_database')
+postgres_user = os.environ.get('postgres_user')
+postgres_password = int(os.environ.get('postgres_password'))
+postgres_port = int(os.environ.get('postgres_port'))
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+
+# Connect to the PostgreSQL server
+
+conn = psycopg2.connect(
+    host=postgres_host,
+    database=postgres_database,
+    user=postgres_user,
+    password=postgres_password,
+    port=postgres_port
+)
+
+# Create a cursor object to execute SQL queries
+cur = conn.cursor()
+
+# Create the table if it doesn't exist
+cur.execute("""SELECT * FROM churn_modelling""")
+
+rows = cur.fetchall()
+
+# Get the column names from the cursor description
+col_names = [desc[0] for desc in cur.description]
+
+# Convert the list of tuples into a Pandas DataFrame
+df = pd.DataFrame(rows, columns=col_names)
 print(df.head())
+
+cur.close()
+conn.close()
