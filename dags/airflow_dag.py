bcfe380dfbd99ad84ce100e727f34cdfbfa7744a
Dogukan Ulu <dogukannulu@gmail.com>
2023-05-07 02:19:57 +0300

    try some dag modification

--- dags/airflow_dag.py
+++ dags/airflow_dag.py
@@ -4,7 +4,8 @@ from datetime import datetime, timedelta
 
 sys.path.append('../')
 
-import write_csv_to_postgres,read_df_from_postgres,write_df_to_postgres,df_modify
+#import write_csv_to_postgres,read_df_from_postgres,write_df_to_postgres,df_modify
+import read_df_from_postgres
 
 
 from airflow.operators.bash import BashOperator
@@ -25,12 +26,17 @@ with DAG('csv_extract_airflow_docker', default_args=default_args, schedule_inter
                                  bash_command='curl -O /Users/dogukanulu/Desktop/codebase/csv_extract_airflow_docker/churn_modelling.csv https://raw.githubusercontent.com/dogukannulu/datasets/master/Churn_Modelling.csv',
                                  retries=1, execution_timeout=timedelta(minutes=10))
 
-    write_csv_to_postgres_task = PythonOperator(task_id='write_csv_to_postgres', python_callable=write_csv_to_postgres)
+    
+    #create_postgres_table_task = PythonOperator(task_id='create_postgres_table', pyhton_callable=create_postgres_table)
+    
+    # write_csv_to_postgres_task = PythonOperator(task_id='write_csv_to_postgres', python_callable=write_csv_to_postgres)
 
-    read_df_from_postgres_task = PythonOperator(task_id='read_df_from_postgres', python_callable=read_df_from_postgres)
+    # read_df_from_postgres_task = PythonOperator(task_id='read_df_from_postgres', python_callable=read_df_from_postgres)
 
-    df_modify_task = PythonOperator(task_id='df_modify', python_callable=df_modify)
+    # df_modify_task = PythonOperator(task_id='df_modify', python_callable=df_modify)
 
-    write_df_to_postgres_task = PythonOperator(task_id='write_df_to_postgres', python_callable=write_df_to_postgres)
+    # write_df_to_postgres_task = PythonOperator(task_id='write_df_to_postgres', python_callable=write_df_to_postgres)
 
-    download_data >> write_csv_to_postgres_task >> read_df_from_postgres_task >> df_modify_task >> write_df_to_postgres_task
+    # download_data >> write_csv_to_postgres_task >> read_df_from_postgres_task >> df_modify_task >> write_df_to_postgres_task
+
+    download_data 
--- write_csv_to_postgres.py
+++ write_csv_to_postgres.py
@@ -12,10 +12,6 @@ postgres_port = int(os.environ.get('postgres_port'))
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
 
-
-
-# Connect to the PostgreSQL server
-
 conn = psycopg2.connect(
     host=postgres_host,
     database=postgres_database,
@@ -24,24 +20,26 @@ conn = psycopg2.connect(
     port=postgres_port
 )
 
-# Create a cursor object to execute SQL queries
 cur = conn.cursor()
 
-# Create the table if it doesn't exist
-cur.execute("""CREATE TABLE IF NOT EXISTS churn_modelling (RowNumber INTEGER PRIMARY KEY, CustomerId INTEGER, 
-Surname VARCHAR(50), CreditScore INTEGER, Geography VARCHAR(50), Gender VARCHAR(20), Age INTEGER, 
-Tenure INTEGER, Balance FLOAT, NumOfProducts INTEGER, HasCrCard INTEGER, IsActiveMember INTEGER, EstimatedSalary FLOAT, Exited INTEGER)""")
+def create_postgres_table():
+    cur.execute("""CREATE TABLE IF NOT EXISTS churn_modelling (RowNumber INTEGER PRIMARY KEY, CustomerId INTEGER, 
+    Surname VARCHAR(50), CreditScore INTEGER, Geography VARCHAR(50), Gender VARCHAR(20), Age INTEGER, 
+    Tenure INTEGER, Balance FLOAT, NumOfProducts INTEGER, HasCrCard INTEGER, IsActiveMember INTEGER, EstimatedSalary FLOAT, Exited INTEGER)""")
 
-# Load the CSV data into the table
-with open('churn_modelling.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    next(csvreader) # skip header row
-    for row in csvreader:
-        cur.execute("""INSERT INTO churn_modelling (RowNumber, CustomerId, Surname, CreditScore, Geography, Gender, Age, 
-        Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary, Exited) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s)""", 
-        (int(row[0]), int(row[1]), str(row[2]), int(row[3]), str(row[4]), str(row[5]), int(row[6]), int(row[7]), float(row[8]), int(row[9]), int(row[10]), int(row[11]), float(row[12]), int(row[13])))
+def write_to_postgres():
+    with open('churn_modelling.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader) # skip header row
+        for row in csvreader:
+            cur.execute("""INSERT INTO churn_modelling (RowNumber, CustomerId, Surname, CreditScore, Geography, Gender, Age, 
+            Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary, Exited) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s)""", 
+            (int(row[0]), int(row[1]), str(row[2]), int(row[3]), str(row[4]), str(row[5]), int(row[6]), int(row[7]), float(row[8]), int(row[9]), int(row[10]), int(row[11]), float(row[12]), int(row[13])))
 
-# Commit the changes and close the cursor and connection
-conn.commit()
-cur.close()
-conn.close()
+
+if __name__ == '__main__':
+    create_postgres_table()
+    write_to_postgres()
+    conn.commit()
+    cur.close()
+    conn.close()
